51cfaebd573e221b920ea6f4be525badfeea9780
Brianfadi <brianfadi20@gmail.com>
2024-11-17 17:08:18 +0300

    Added django portfolio files including index.html

---
+++ portfolio/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from .models import ContactMessage
+
+# Create your views here.
+
+def main(request):
+    return render(request, 'index.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        ContactMessage.objects.create(name=name, email=email, phone=phone, subject=subject, message=message)
+        return redirect('contact')
+
+    return render(request, 'contact.html')
